ab00afa85984de7a80e807663291679ca097899a
mohammedsobhee <mohammedsobhee@gmail.com>
2020-09-09 22:46:44 +0300

    A

--- app/Http/Requests/Api/Offer/CreateRequest.php
+++ app/Http/Requests/Api/Offer/CreateRequest.php
@@ -26,8 +26,8 @@ class CreateRequest extends FormRequest
         return [
             //`request_id`, `service_provider_id`, `status`, `payment_type`, `payment_value`, `details`
             'request_id' => 'required|exists:requests,id,status,new',
-            'payment_type' => 'required|in:down_payment,late_payment',
-            'payment_value' => 'required|numeric',
+            'down_payment' => 'required|numeric|gte:0',
+            'late_payment' => 'required|numeric|gte:0',
         ];
     }
 }
