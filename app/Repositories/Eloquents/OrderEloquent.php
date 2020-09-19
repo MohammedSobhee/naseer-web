273f7fbe88d6c3d92c78a4f136cb1bcb6e1679a3
mohammedsobhee <mohammedsobhee@gmail.com>
2020-09-19 15:24:58 +0300

    A

--- app/Repositories/Eloquents/OrderEloquent.php
+++ app/Repositories/Eloquents/OrderEloquent.php
@@ -438,43 +438,42 @@ class OrderEloquent extends Uploader implements Repository
                     sleep(1);
                     $request->case_file = $this->upload($attributes, 'case_file');
                 } else if (isset($attributes['case_file']) && $attributes['case_file'] == 'deleted') {
-                    unlink(base_path($request->getAttributes()['case_file']));
+                    unlink(base_path('assets/upload/' . $request->getAttributes()['case_file']));
                     $request->case_file = null;
                 }
                 if (isset($attributes['evidences_file'])) {
                     sleep(1);
                     $request->evidences_file = $this->upload($attributes, 'evidences_file');
                 } else if (isset($attributes['evidences_file']) && $attributes['evidences_file'] == 'deleted') {
-                    unlink(base_path($request->getAttributes()['evidences_file']));
-
+                    unlink(base_path('assets/upload/' . $request->getAttributes()['evidences_file']));
                     $request->evidences_file = null;
                 }
                 if (isset($attributes['preferred_outcomes_file'])) {
                     sleep(1);
                     $request->preferred_outcomes_file = $this->upload($attributes, 'preferred_outcomes_file');
                 } else if (isset($attributes['preferred_outcomes_file']) && $attributes['preferred_outcomes_file'] == 'deleted') {
-                    unlink(base_path($request->getAttributes()['preferred_outcomes_file']));
+                    unlink(base_path('assets/upload/' . $request->getAttributes()['preferred_outcomes_file']));
                     $request->preferred_outcomes_file = null;
                 }
                 if (isset($attributes['case_audio'])) {
                     sleep(1);
                     $request->case_audio = $this->upload($attributes, 'case_audio');
                 } else if (isset($attributes['case_audio']) && $attributes['case_audio'] == 'deleted') {
-                    unlink(base_path($request->getAttributes()['case_audio']));
+                    unlink(base_path('assets/upload/' . $request->getAttributes()['case_audio']));
                     $request->case_audio = null;
                 }
                 if (isset($attributes['evidences_audio'])) {
                     sleep(1);
                     $request->evidences_audio = $this->upload($attributes, 'evidences_audio');
                 } else if (isset($attributes['evidences_audio']) && $attributes['evidences_audio'] == 'deleted') {
-                    unlink(base_path($request->getAttributes()['evidences_audio']));
+                    unlink(base_path('assets/upload/' . $request->getAttributes()['evidences_audio']));
                     $request->evidences_audio = null;
                 }
                 if (isset($attributes['preferred_outcomes_audio'])) {
                     sleep(1);
                     $request->preferred_outcomes_audio = $this->upload($attributes, 'preferred_outcomes_audio');
                 } else if (isset($attributes['preferred_outcomes_audio']) && $attributes['preferred_outcomes_audio'] == 'deleted') {
-                    unlink(base_path($request->getAttributes()['preferred_outcomes_audio']));
+                    unlink(base_path('assets/upload/' . $request->getAttributes()['preferred_outcomes_audio']));
                     $request->preferred_outcomes_audio = null;
                 }
                 $request->save();
