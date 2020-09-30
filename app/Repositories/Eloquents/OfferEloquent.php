1fe828ed367f7a04ee15d51e2c955b93f27c9ffd
mohammedsobhee <mohammedsobhee@gmail.com>
2020-09-30 17:43:31 +0300

    A

--- app/Repositories/Eloquents/OfferEloquent.php
+++ app/Repositories/Eloquents/OfferEloquent.php
@@ -165,7 +165,7 @@ class OfferEloquent extends Uploader implements Repository
 
             if ($attributes['status'] == 'accepted') {
                 $offer->Order()->update(['status' => 'assigned', 'is_edit' => 1]);
-                $this->notification->sendNotification(auth()->user()->id, $offer->Order->user_id, $offer->request_id, 'assigned_driver');
+                $this->notification->sendNotification(auth()->user()->id, $offer->service_provider_id, $offer->request_id, 'assigned_driver');
 
             }
             return response_api(true, 200, null, []);
