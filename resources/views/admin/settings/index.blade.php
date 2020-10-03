79554a5ebf82df67a87427a2269fb74fe194df5d
mohammedsobhee <mohammedsobhee@gmail.com>
2020-10-03 19:39:19 +0300

    3-10-2020

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -31,14 +31,14 @@
                     <div class="form-group">
                         <label for="support_mobile" class="col-md-2 control-label">وقت انتهاء صلاحية العرض</label>
                         <div class="col-md-6">
-                            <input type="text" class="form-control" value="{{$setting->expire_offer ?? ''}}" name="expire_offer" id="expire_offer" placeholder="وقت انتهاء صلاحية العرض">
+                            <input type="text" class="form-control " value="{{$setting->expire_offer ?? ''}}" name="expire_offer" id="expire_offer" placeholder="وقت انتهاء صلاحية العرض">
                         </div>
                     </div>
 
                     <div class="form-group">
                         <label for="contract" class="col-md-2 control-label">تفاصيل عقد الاتفاق</label>
                         <div class="col-md-6">
-                            <textarea rows="4"  class="form-control" name="contract" placeholder="تفاصيل عقد الاتفاق">{{$setting->contract ?? ''}}
+                            <textarea rows="5"  class="form-control ckeditor" name="contract" placeholder="تفاصيل عقد الاتفاق">{{$setting->contract ?? ''}}
                             </textarea>
                         </div>
                     </div>
@@ -46,15 +46,19 @@
                     <div class="form-group">
                         <label for="terms" class="col-md-2 control-label">الشروط والاحكام</label>
                         <div class="col-md-6">
-                            <textarea  rows="4" class="form-control" name="terms"  id="terms"
+                            <textarea  rows="5" class="form-control ckeditor" name="terms"  id="terms"
                                    placeholder="الشروط والاحكام">{{$setting->terms ?? ''}}
                             </textarea>
                         </div>
                     </div>
-
-                    <div class="form-group">
-                        <div class="col-md-8 text-center">
-                            <button type="submit" class="btn green save"> Save<i class="fa fa-save"></i></button>
+                    <div class="form-actions">
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <button type="submit" class="btn btn-circle green btn-md save"><i
+                                        class="fa fa-check"></i>
+                                    حفظ
+                                </button>
+                            </div>
                         </div>
                     </div>
 
@@ -86,5 +90,6 @@
     <script src="{{url('/')}}/assets/pages/scripts/table-datatables-managed.min.js"
             type="text/javascript"></script>
     <script src="{{url('/')}}/assets/js/setting.js" type="text/javascript"></script>
+    <script src="{{url('/')}}/assets/global/plugins/ckeditor/ckeditor.js" type="text/javascript"></script>
 
 @stop
