1e6ad05bf7957a0149dccb3c1a8c8b7db8fdf06e
mohammedsobhee <mohammedsobhee@gmail.com>
2020-10-11 01:37:32 +0300

    A

--- resources/views/admin/users/add-provider.blade.php
+++ resources/views/admin/users/add-provider.blade.php
@@ -288,7 +288,7 @@
 
     <script>
     $(document).ready(function () {
-
+        initGeolocation();
     });
 
     function AddPlace() {
@@ -587,6 +587,35 @@
 
         $('#exampleModalLong').modal('show');
     }
+
+
+    function initGeolocation()
+    {
+        if( navigator.geolocation )
+        {
+            // Call getCurrentPosition with success and failure callbacks
+            navigator.geolocation.getCurrentPosition( success, fail );
+        }
+        else
+        {
+            alert("Sorry, your browser does not support geolocation services.");
+        }
+    }
+
+    function success(position)
+    {
+
+        console.log(position.coords.latitude)
+        console.log(position.coords.longitude)
+        // document.getElementById('long').value = position.coords.longitude;
+        // document.getElementById('lat').value = position.coords.latitude
+    }
+
+    function fail()
+    {
+        // Could not obtain location
+    }
+
     </script>
 
     <script
