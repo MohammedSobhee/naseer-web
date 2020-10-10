1016201818af9f18bca237defdbfc331379bca2f
mohammedsobhee <mohammedsobhee@gmail.com>
2020-10-11 02:02:55 +0300

    A

--- resources/views/admin/users/add-provider.blade.php
+++ resources/views/admin/users/add-provider.blade.php
@@ -261,7 +261,7 @@
     </div>
 
 
-    <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
+    <div class="modal fade" id="exampleModalLong" role="dialog" aria-labelledby="exampleModalLongTitle"
          aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
