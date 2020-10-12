17e3b3bed5646c20f10390e85989b442b6735e5d
mohammedsobhee <mohammedsobhee@gmail.com>
2020-10-12 13:54:05 +0300

    AddServiceProvider

--- resources/views/admin/users/edit-provider.blade.php
+++ resources/views/admin/users/edit-provider.blade.php
@@ -303,11 +303,11 @@
                         <!--/span-->
                     </div>
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-6">
                             <div class="form-group">
                                 <label class="control-label col-md-3">نبذه:</label>
                                 <div class="col-md-9">
-                                    <textarea name="bio" id="bio" rows="5" placeholder="اضف نبذه..."
+                                    <textarea name="bio" id="bio" rows="10" placeholder="اضف نبذه..."
                                               class="form-control">{{$user->ServiceProvider->bio}}</textarea>
                                 </div>
                             </div>
