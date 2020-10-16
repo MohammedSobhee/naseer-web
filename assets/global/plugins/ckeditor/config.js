52822c38cce56d63c482738af880a618942664ae
mohammedsobhee <mohammedsobhee@gmail.com>
2020-10-16 14:54:55 +0300

    A

--- assets/global/plugins/ckeditor/config.js
+++ assets/global/plugins/ckeditor/config.js
@@ -7,4 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+
+    config.width="100%";
+    config.height="1000px"
 };
