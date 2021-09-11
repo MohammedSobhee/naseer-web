a4c03ea8b3fe783d5805343c4976c92b6c43f016
mohammedsobhee <mohammedsobhee@gmail.com>
2021-09-11 14:48:18 +0300

    ...

--- app/Repositories/Eloquents/OrderEloquent.php
+++ app/Repositories/Eloquents/OrderEloquent.php
@@ -366,7 +366,7 @@ class OrderEloquent extends Uploader implements Repository
 
             $request = $request->fresh();
             // cancel request after 24 hours when request is new (no offer accepted)
-            $job = (new RejectPendingOfferJob($request))->delay(Carbon::now()->addMinutes(1));
+            $job = (new RejectPendingOfferJob($request))->delay(Carbon::now()->addSeconds(10));
             $jobId = app(Dispatcher::class)->dispatch($job);
             return response_api(true, 200, 'تم انشاء الطلب بنجاح', new OrderResource($request));// . ',' . trans('app.sent_email_verification')
         }
