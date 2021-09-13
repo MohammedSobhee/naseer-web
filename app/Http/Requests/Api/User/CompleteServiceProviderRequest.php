4cd08919f57ecc69796307b7c11247896b392d3a
mohammedsobhee <mohammedsobhee@gmail.com>
2021-09-13 22:54:18 +0300

    ...

--- app/Http/Requests/Api/User/CompleteServiceProviderRequest.php
+++ app/Http/Requests/Api/User/CompleteServiceProviderRequest.php
@@ -27,15 +27,15 @@ class CompleteServiceProviderRequest extends FormRequest
             //
             'service_provider_type_id' => 'required|exists:service_provider_types,id',
             'idno' => 'required',
-            'idno_file' => 'required',
+            'idno_file' => 'required|file',
             'skill' => 'required',
             'photo' => 'nullable|image',
-            'skill_file' => 'required',
+            'skill_file' => 'required|file',
             'address' => 'required',
             'latitude' => 'required',
             'longitude' => 'required',
             'licensed' => 'nullable',
-//            'licensed_file' => 'nullable',
+            'licensed_file' => 'nullable|file',
         ];
     }
 }
