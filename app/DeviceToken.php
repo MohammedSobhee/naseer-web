9b4f7aac027c8bc27d3e7c359fe718b5f39f2128
mohammedsobhee <mohammedsobhee@gmail.com>
2021-09-16 18:56:00 +0300

    ...

--- app/DeviceToken.php
+++ app/DeviceToken.php
@@ -14,8 +14,8 @@ class DeviceToken extends Model
 
     static function getReceiverToken($receiver_id)
     {
-        $token_android = self::where('user_id', $receiver_id)->where('type', 'android')->pluck('device_token')->toArray();
-        $token_ios = self::where('user_id', $receiver_id)->where('type', 'ios')->pluck('device_token')->toArray();
+        $token_android = self::where('user_id', $receiver_id)->where('status', 'on')->where('type', 'android')->pluck('device_token')->toArray();
+        $token_ios = self::where('user_id', $receiver_id)->where('status', 'on')->where('type', 'ios')->pluck('device_token')->toArray();
 
         return [$token_android, $token_ios];
     }
@@ -23,8 +23,8 @@ class DeviceToken extends Model
     static function getDevices($receiver_id)
     {
         $devices = [];
-        $device_android_id = self::where('user_id', $receiver_id)->where('type', 'android')->orderByDesc('updated_at')->first();
-        $device_ios_id = self::where('user_id', $receiver_id)->where('type', 'ios')->orderByDesc('updated_at')->first();
+        $device_android_id = self::where('user_id', $receiver_id)->where('status', 'on')->where('type', 'android')->orderByDesc('updated_at')->first();
+        $device_ios_id = self::where('user_id', $receiver_id)->where('status', 'on')->where('type', 'ios')->orderByDesc('updated_at')->first();
 
         if ($device_android_id) {
             $devices [] = $device_android_id->device_id;
